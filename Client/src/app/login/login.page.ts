b355ea8b1fea7a5d46d2d6681bd36ced6930bd51
Daniel Verhagen <dver99@sbcglobal.net>
2021-09-04 16:39:33 -0700

    env url

--- Client/src/app/login/login.page.ts
+++ Client/src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 import { AuthenicateService } from '../authenicate.service';
 
 @Component({
@@ -14,7 +15,7 @@ export class LoginPage implements OnInit {
   public responseTypeToken = 'token';
   public responseTypeCode = 'code';
   public redirectUriFront = 'http://localhost:8100/callback';
-  public redirectUriBack = 'https://localhost:44397/Queue/Callback';
+  public redirectUriBack = environment.apiUrl + '/Queue/Callback';
   public state = 'secret';
   public scope = "user-modify-playback-state user-read-playback-state user-read-playback-position user-read-private user-read-email playlist-read-private user-library-read user-library-modify user-top-read playlist-read-collaborative playlist-modify-public playlist-modify-private ugc-image-upload user-follow-read user-follow-modify user-modify-playback-state user-read-currently-playing user-read-recently-played";
   public readonly authorizeUrl = 'https://accounts.spotify.com/authorize?';
