0f0a65a92b2ae5f04c110bae0e9a1b8961a65a68
Daniel Verhagen <dver99@sbcglobal.net>
2022-07-27 23:55:07 -0700

    CORS update prod url/appsettings

--- Server/Server/Startup.cs
+++ Server/Server/Startup.cs
@@ -23,7 +23,7 @@ namespace Server
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            origins = new string[]{ "https://localhost:44397", "http://localhost:5000", "http://localhost:8100", "https://party-queue-api.azurewebsites.net", "https://ambitious-grass-0ffb2921e.azurestaticapps.net" };
+            origins = new string[]{ "https://localhost:44397", "http://localhost:5000", "http://localhost:8100", Configuration["URL"] };
         }
 
         public IConfiguration Configuration { get; }
