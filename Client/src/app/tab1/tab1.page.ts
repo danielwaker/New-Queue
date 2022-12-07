3b74974f74f754a2123edc722ac5f2f26b98938a
Daniel Verhagen <dver99@sbcglobal.net>
2022-12-06 20:18:09 -0700

    remove debugging leader
    lol I just didn't deploy the server changes from 4 months ago

--- Client/src/app/tab1/tab1.page.ts
+++ Client/src/app/tab1/tab1.page.ts
@@ -126,8 +126,6 @@ export class Tab1Page {
         localStorage.removeItem("sessionId");
       } else {
         this.leader = this.users[localStorage.getItem('user')].Leader;
-        this.log('user: ' + localStorage.getItem('user'));
-        this.log('leader: ' + this.users[localStorage.getItem('user')].Leader);
         this.sessionStatus = (this.leader) ? SessionEnum.End : SessionEnum.Leave;
         if (getQueue) {
           this.getQueue();
