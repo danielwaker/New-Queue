daf821e4202c308f08f8c559cf2675843e9b6822
Daniel Verhagen <dver99@sbcglobal.net>
2023-02-23 20:20:55 -0700

    start styling

--- Client-React-Ionic/src/pages/Tab1.tsx
+++ Client-React-Ionic/src/pages/Tab1.tsx
@@ -1,21 +1,21 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { refreshCircleOutline } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader collapse="condense">
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          {/* <IonTitle size="large">Tab 1</IonTitle> */}
+          <IonButton fill="clear" slot="end">
+            <IonIcon icon={refreshCircleOutline}></IonIcon>
+          </IonButton>
         </IonToolbar>
-      </IonHeader>
+      </IonHeader>      
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+        <IonButton>Create Session</IonButton>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
--- Client-React-Ionic/src/pages/Tab2.tsx
+++ Client-React-Ionic/src/pages/Tab2.tsx
@@ -1,21 +1,55 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { closeCircleOutline, searchOutline } from 'ionicons/icons';
+import { useRef, useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
+  const [inputModel, setInputModel ] = useState('');
+  const ionInputEl = useRef<HTMLIonInputElement>(null);
+  
+  const onInput = (ev: Event) => {
+    const value = (ev.target as HTMLIonInputElement).value as string;
+    
+    // Removes non alphanumeric characters
+    const filteredValue = value.replace(/[^a-zA-Z0-9]+/g,'');
+    
+    /**
+     * Update both the state variable and
+     * the component to keep them in sync.
+     */
+    setInputModel(filteredValue);
+    
+    const inputCmp = ionInputEl.current;
+    if (inputCmp !== null) {
+      inputCmp.value = filteredValue;
+    }
+  }
+
+  function clear() {
+    
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Search Songs</IonTitle>
+        </IonToolbar>
+        <IonToolbar>
+          <IonItem class="search">
+            <IonInput value={inputModel} onIonInput={onInput} ref={ionInputEl}>
+              <IonButton>
+                <IonIcon class="search-icon" icon={searchOutline}></IonIcon>
+              </IonButton>
+            </IonInput>
+            <IonButton fill="clear" onClick={clear}>
+              <IonIcon class="x-icon" icon={closeCircleOutline}></IonIcon>
+            </IonButton>
+          </IonItem>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <ExploreContainer name="Tab 2 page" />
       </IonContent>
     </IonPage>
