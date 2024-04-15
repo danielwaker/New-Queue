ffcf1b9435bc40f4b6f7d5a1ce6101256478f85d
Daniel Verhagen <dver99@sbcglobal.net>
2024-04-15 02:09:40 -0700

    angular 13 upgrade, npm audit fix

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -10,10 +10,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BackgroundMode],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, BackgroundMode],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
